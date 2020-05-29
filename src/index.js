89c96c6f3ebc556bd618b9123e73f3c37e030e18
m-malanowski <info@mateusz-malanowski.pl>
2020-05-29 12:02:34 +0200

    first commit lumina studio without react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import './scss/app.scss';
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.(jpe?g|png|ico|gif|svg)$/,
+                loader: "file-loader",
+                query: {
+                    name: 'img/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath:  './src/assets/fonts'
+                        }
+                    }
+                     ]
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    },
+                ]
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: __dirname + '/public',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './public'
+    }
+};
